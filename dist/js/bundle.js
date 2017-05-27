28573a14db81bd35582e3123a8950a88440447b0
Your Name <you@example.com>
2017-05-27 14:26:18 -0600

    CSS Improvments

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -18,42 +18,6 @@ angular.module("app", ["ui.router"]).config(function ($stateProvider, $urlRouter
 });
 "use strict";
 
-angular.module("app").service("mainSvc", function ($http) {
-
-    var getAlbums = function getAlbums() {
-        return $http({
-            method: "GET",
-            url: 'https://api.spotify.com/v1/albums/4OHNH3sDzIxnmUADXzv2kT,0aGwrXjKIfAMlj1vBYLtnR' //<--- this is a bad request
-        }).then(function (res) {
-            return res.data;
-        });
-    }; // <--- you were missing a brace, which was
-
-    var getArtist = function getArtist() {
-        return $http({
-            method: "GET",
-            url: 'https://api.spotify.com/v1/artists?ids=0oSGxfWSnnOXhD2fKuz2Gy,3dBVyJ7JuOMt4GE9607Qin3kjuyTCjPG1WMFCiyc5IuB,artist/0C0XlULifJtAgn6ZNCW2eu'
-        });
-    };
-    var getTracks = function getTracks() {
-        return $http({
-            method: "GET",
-            url: "https://api.spotify.com/v1/albums/6akEvsycLGftJxYudPjmqK/tracks?limit=2"
-        });
-    };
-    var getAllData = function getAllData() {
-        getArtist().then(function (artists) {
-            getTracks().then(function (tracks) {
-                getAlbums().then(function (albums) {
-                    console.log(artists, tracks, albums);
-                });
-            });
-        });
-    };
-    getAllData();
-});
-"use strict";
-
 angular.module("app").controller("mainCtrl", function ($scope, mainSvc) {
 
     // $(".cross").hide();
@@ -92,3 +56,39 @@ angular.module("app").controller("mainCtrl", function ($scope, mainSvc) {
     };
     $scope.getArtistsAndTracks();
 }); // <--- end of controller
+"use strict";
+
+angular.module("app").service("mainSvc", function ($http) {
+
+    var getAlbums = function getAlbums() {
+        return $http({
+            method: "GET",
+            url: 'https://api.spotify.com/v1/albums/4OHNH3sDzIxnmUADXzv2kT,0aGwrXjKIfAMlj1vBYLtnR' //<--- this is a bad request
+        }).then(function (res) {
+            return res.data;
+        });
+    }; // <--- you were missing a brace, which was
+
+    var getArtist = function getArtist() {
+        return $http({
+            method: "GET",
+            url: 'https://api.spotify.com/v1/artists?ids=0oSGxfWSnnOXhD2fKuz2Gy,3dBVyJ7JuOMt4GE9607Qin3kjuyTCjPG1WMFCiyc5IuB,artist/0C0XlULifJtAgn6ZNCW2eu'
+        });
+    };
+    var getTracks = function getTracks() {
+        return $http({
+            method: "GET",
+            url: "https://api.spotify.com/v1/albums/6akEvsycLGftJxYudPjmqK/tracks?limit=2"
+        });
+    };
+    var getAllData = function getAllData() {
+        getArtist().then(function (artists) {
+            getTracks().then(function (tracks) {
+                getAlbums().then(function (albums) {
+                    console.log(artists, tracks, albums);
+                });
+            });
+        });
+    };
+    getAllData();
+});
